c601ccf3c3383e4f5bf3f964188700756d901f33
Soan Trinh <tmsoan@gmail.com>
2023-08-21 14:57:25 +0700

    [Core][Network] Add `pretty_dio_logger`

--- data/lib/src/repository/repository_impl.dart
+++ data/lib/src/repository/repository_impl.dart
@@ -1,17 +1,32 @@
 import 'package:dio/dio.dart';
 import 'package:domain/domain.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:shared/shared.dart';
 
 import '../../data.dart';
 
 class RepositoryImpl implements Repository {
-  final RestClient _restClient = RestClient(Dio());
+
+  Dio _buildDioClient() {
+    final dio = Dio();
+    dio.interceptors.add(PrettyDioLogger(
+        requestHeader: true,
+        requestBody: true,
+        responseBody: true,
+        responseHeader: false,
+        error: true,
+        compact: true,
+        maxWidth: 90));
+    return dio;
+  }
+
+  late RestClient _restClient = RestClient(
+      _buildDioClient()
+  );
 
   @override
   Future<void> login(String email, String password) async {
     kEZLogger.e('`data layer` API calling...');
-    _restClient.getTasks().then((value) => {
-      value.forEach((element) {kEZLogger.d(element.toJson());})
-    });
+    _restClient.getTasks().then((value) => kEZLogger.d(value));
   }
 }
